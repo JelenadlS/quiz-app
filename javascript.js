d25963c0fefac08faff632f588e9bdf738c51cd5
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-01-26 20:15:00 +0100

    Add JS to bookmark

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const BookmarkElement = document.querySelector('[data-js="bookmark"]');
+const ButtonToggle = document.querySelector('[data-js="button-toggle" ]');
+
+ButtonToggle.addEventListener('click', () => {
+  BookmarkElement.classList.toggle('card__bookmark--active');
+});
