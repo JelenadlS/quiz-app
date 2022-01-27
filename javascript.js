ec609bb119800e43272e3abc6c575375e764dca1
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-01-27 10:26:28 +0100

    Add JS to showAnser

--- javascript.js
+++ javascript.js
@@ -4,3 +4,15 @@ const ButtonToggle = document.querySelector('[data-js="button-toggle" ]');
 ButtonToggle.addEventListener('click', () => {
   BookmarkElement.classList.toggle('card__bookmark--active');
 });
+
+const answerButton = document.querySelector('[data-js="show-answer"]');
+const hideButton = document.querySelector('[data-js="hide-answer"]');
+
+answerButton.addEventListener('click', () => {
+  hideButton.classList.toggle('hide');
+  if (answerButton.textContent === 'Hide answer') {
+    answerButton.textContent = 'Show answer';
+  } else {
+    answerButton.textContent = 'Hide answer';
+  }
+});
