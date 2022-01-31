0c32d2c6ac999527fb530a0b17978b09b0f2d5f9
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-01-31 14:07:48 +0100

    Fix Card error and shorten navigation.js

--- js/Card.js
+++ js/Card.js
@@ -1,13 +1,13 @@
 export default function Card(cardElement) {
-  const BookmarkElement = document.querySelector('[data-js="bookmark"]');
-  const ButtonToggle = document.querySelector('[data-js="button-toggle" ]');
+  const BookmarkElement = cardElement.querySelector('[data-js="bookmark"]');
+  const ButtonToggle = cardElement.querySelector('[data-js="button-toggle"]');
 
   ButtonToggle.addEventListener('click', () => {
     BookmarkElement.classList.toggle('card__bookmark--active');
   });
 
-  const answerButton = document.querySelector('[data-js="show-answer"]');
-  const hideButton = document.querySelector('[data-js="hide-answer"]');
+  const answerButton = cardElement.querySelector('[data-js="show-answer"]');
+  const hideButton = cardElement.querySelector('[data-js="hide-answer"]');
 
   answerButton.addEventListener('click', () => {
     hideButton.classList.toggle('hide');
--- js/Navigation.js
+++ js/Navigation.js
@@ -1,40 +1,17 @@
 export default function Nav() {
-  const homeButton = document.querySelector('[data-js="home-button"]');
-  const homeContent = document.querySelector('[data-js="home-content"]');
-  const createButton = document.querySelector('[data-js="create-button"]');
-  const createContent = document.querySelector('[data-js="create-content"]');
-  const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
-  const bookmarkContent = document.querySelector(
-    '[data-js="bookmark-content"]'
-  );
-  const profileButton = document.querySelector('[data-js="profile-button"]');
-  const profileContent = document.querySelector('[data-js="profile-content"]');
+  const allNavIcons = document.querySelectorAll('[data-nav]');
+  const allPages = document.querySelectorAll('[data-page]');
 
-  homeButton.addEventListener('click', () => {
-    homeContent.classList.remove('hide');
-    createContent.classList.add('hide');
-    bookmarkContent.classList.add('hide');
-    profileContent.classList.add('hide');
-  });
-
-  createButton.addEventListener('click', () => {
-    createContent.classList.remove('hide');
-    homeContent.classList.add('hide');
-    bookmarkContent.classList.add('hide');
-    profileContent.classList.add('hide');
-  });
-
-  bookmarkButton.addEventListener('click', () => {
-    bookmarkContent.classList.remove('hide');
-    homeContent.classList.add('hide');
-    createContent.classList.add('hide');
-    profileContent.classList.add('hide');
-  });
-
-  profileButton.addEventListener('click', () => {
-    profileContent.classList.remove('hide');
-    homeContent.classList.add('hide');
-    createContent.classList.add('hide');
-    bookmarkContent.classList.add('hide');
+  allNavIcons.forEach((clickedIcon) => {
+    clickedIcon.addEventListener('click', (event) => {
+      allPages.forEach((otherPages) => {
+        otherPages.classList.add('hide');
+      });
+      const iconAttribute = event.target.getAttribute('data-nav');
+      const associatedPage = document.querySelector(
+        `[data-page="${iconAttribute}"]`
+      );
+      associatedPage.classList.remove('hide');
+    });
   });
 }
