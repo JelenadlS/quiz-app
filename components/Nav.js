5a2b961ccba8e069a43a65c8168c5fda05c1b269
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-02-08 20:36:32 +0100

    Add active button and changing title with NavBar

--- components/Nav.js
+++ components/Nav.js
@@ -1,17 +1,32 @@
 export default function Nav() {
   const allNavIcons = document.querySelectorAll('[data-nav]');
   const allPages = document.querySelectorAll('[data-page]');
+  const allTitles = document.querySelectorAll('[data-title]');
 
   allNavIcons.forEach((clickedIcon) => {
     clickedIcon.addEventListener('click', (event) => {
-      allPages.forEach((pages) => {
-        pages.classList.add('hide');
+      allPages.forEach((page) => {
+        page.classList.add('hide');
+      });
+      allNavIcons.forEach((NavIcon) => {
+        NavIcon.classList.remove('Nav__icon--active');
+      });
+      allTitles.forEach((title) => {
+        title.classList.add('hide');
       });
       const iconAttribute = event.target.getAttribute('data-nav');
+      const associatedIcon = document.querySelector(
+        `[data-nav="${iconAttribute}"]`
+      );
       const associatedPage = document.querySelector(
         `[data-page="${iconAttribute}"]`
       );
+      const associatedTitle = document.querySelector(
+        `[data-title="${iconAttribute}"]`
+      );
+      associatedTitle.classList.remove('hide');
       associatedPage.classList.remove('hide');
+      associatedIcon.classList.add('Nav__icon--active');
     });
   });
 }
