49c812e1cd6581b64a8a116b9c59eeecfcde8a96
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-02-13 18:06:26 +0100

    Add old card classes to new card element created via JS

--- components/Create.js
+++ components/Create.js
@@ -7,7 +7,6 @@ export default function Create() {
   const charactersLeftAnswer = document.querySelector(
     '[data-js="characters-answer"]'
   );
-
   const cardsContainer = document.querySelector('[data-js=cards]');
   const form = document.querySelector('[data-js=form]');
 
@@ -61,11 +60,11 @@ export default function Create() {
   function renderCards() {
     cardsContainer.ariaBusy = 'true';
     cardsContainer.innerHTML = '';
-    cards.forEach((card) => {
+    cards.forEach((card, index) => {
       const cardElement = document.createElement('li');
       cardElement.className = 'card';
       cardElement.innerHTML = `
-        <p>${card.question}</p>
+        <p class="center pdb-10">${card.question}</p>
         <button 
         class="card__bookmark${
           card.isBookmarked ? ' card__bookmark--active' : ''
@@ -74,10 +73,18 @@ export default function Create() {
         aria-pressed="${card.isBookmarked ? 'true' : 'false'}"
         data-js="bookmark">
       </button>
-        <p>${card.answer}</p>
-        <ul role="list" class="card__tag-list">
+      <button
+      data-js="card-button"
+      aria-controls="answer${index}"
+      aria-expanded="false"
+      class="card__answer card__answer--pointer"
+    >Show answer</button>
+    <p id="answer${index}" data-js="answer" hidden class="center mgt-10">${
+        card.answer
+      }</p>
+        <ul role="list" class="card__tags no-bul">
           ${card.tags
-            .map((tag) => `<li class="card__tag">${tag}</li>`)
+            .map((tag) => `<li class="inline card__tags--style">${tag}</li>`)
             .join('')}
         </ul>
       `;
@@ -89,6 +96,16 @@ export default function Create() {
         card.isBookmarked = !card.isBookmarked;
         bookmarkElement.classList.toggle('card__bookmark--active');
         bookmarkElement.setAttribute('aria-pressed', card.isBookmarked);
+      });
+
+      const answerButton = cardElement.querySelector('[data-js=card-button]');
+      const answerElement = cardElement.querySelector('[data-js=answer]');
+
+      answerButton.addEventListener('click', () => {
+        answerElement.toggleAttribute('hidden');
+        const oldExpandedState = answerButton.getAttribute('aria-expanded');
+        const newExpandedState = oldExpandedState === 'true' ? 'false' : 'true';
+        answerButton.setAttribute('aria-expanded', newExpandedState);
       });
     });
     cardsContainer.ariaBusy = 'false';
