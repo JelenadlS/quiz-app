47da5ea7882c30d9e1ae15a31c413de921980c7a
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-02-13 17:40:54 +0100

    Add create form and card with toggle bookmark via JS

--- components/Create.js
+++ components/Create.js
@@ -8,6 +8,11 @@ export default function Create() {
     '[data-js="characters-answer"]'
   );
 
+  const cardsContainer = document.querySelector('[data-js=cards]');
+  const form = document.querySelector('[data-js=form]');
+
+  let cards = [];
+
   textQuestion.addEventListener('input', () => {
     const textLength = textQuestion.value.length;
     charactersLeftQuestion.value = 150 - textLength;
@@ -30,4 +35,62 @@ export default function Create() {
         charactersLeftAnswer.value + ' characters remaining';
     }
   });
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const questionElement = form.elements.question;
+    const answerElement = form.elements.answer;
+    const tagsElement = form.elements.tags;
+
+    const newCard = {
+      question: questionElement.value,
+      answer: answerElement.value,
+      tags: tagsElement.value
+        .split(',')
+        .map((tag) => tag.trim())
+        .filter((tag) => tag.length),
+    };
+
+    cards = [newCard, ...cards];
+    renderCards();
+
+    form.reset();
+  });
+
+  function renderCards() {
+    cardsContainer.ariaBusy = 'true';
+    cardsContainer.innerHTML = '';
+    cards.forEach((card) => {
+      const cardElement = document.createElement('li');
+      cardElement.className = 'card';
+      cardElement.innerHTML = `
+        <p>${card.question}</p>
+        <button 
+        class="card__bookmark${
+          card.isBookmarked ? ' card__bookmark--active' : ''
+        }"
+        aria-label="Bookmark"
+        aria-pressed="${card.isBookmarked ? 'true' : 'false'}"
+        data-js="bookmark">
+      </button>
+        <p>${card.answer}</p>
+        <ul role="list" class="card__tag-list">
+          ${card.tags
+            .map((tag) => `<li class="card__tag">${tag}</li>`)
+            .join('')}
+        </ul>
+      `;
+      cardsContainer.append(cardElement);
+
+      const bookmarkElement = cardElement.querySelector('[data-js=bookmark]');
+
+      bookmarkElement.addEventListener('click', () => {
+        card.isBookmarked = !card.isBookmarked;
+        bookmarkElement.classList.toggle('card__bookmark--active');
+        bookmarkElement.setAttribute('aria-pressed', card.isBookmarked);
+      });
+    });
+    cardsContainer.ariaBusy = 'false';
+  }
 }
